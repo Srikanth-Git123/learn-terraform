4b91b0fb889ba54be50650350a00cbccf9ede27e
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-03-25 15:28:42 +0530

    Create expense project instances

---
+++ expense-v1/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "frontend" {
+  ami                    = "ami-036c2987dfef867fb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = "sg-0605b31229658744d"
+  tags = {
+    Name = "frontend"
+  }
+}
+
+
+
+resource "aws_instance" "backend" {
+  ami                    = "ami-036c2987dfef867fb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = "sg-0605b31229658744d"
+  tags = {
+    Name = "backend"
+  }
+}
+
+
+
+resource "aws_instance" "mysql" {
+  ami                    = "ami-036c2987dfef867fb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = "sg-0605b31229658744d"
+  tags = {
+    Name = "mysql"
+  }
+}
