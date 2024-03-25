7117fff6a16a158ef48e75157214cbbf93e902ec
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-03-25 15:52:27 +0530

    Var demo

--- 03-variable/var.tf
+++ 03-variable/var.tf
@@ -7,8 +7,27 @@ variable "x" {
 # Declare variable without value
 #variable "y" {}
 
+variable "y_list" {
+  default = [1,2,3]
+}
 
-# Print ariable
+variable "z_map" {
+  default = {
+    x = 10
+    y = 20
+    z = 30
+  }
+}
+
+# Print variable
 output "x" {
   value = var.x
+}
+
+output "y" {
+  value = var.y_list
+}
+
+output "z" {
+  value = var.z_map
 }
